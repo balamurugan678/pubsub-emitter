999f0fd0088424ab57c66000b135760a20544051
balamurugan678@yahoo.co.in <GBala678#>
2017-07-18 17:43:14 +0100

    Second Commit

--- src/main/java/com/sample/poc/PubSubEmitController.java
+++ src/main/java/com/sample/poc/PubSubEmitController.java
@@ -1,27 +1,13 @@
 package com.sample.poc;
 
-import com.google.cloud.bigtable.hbase.BigtableConfiguration;
-import com.google.cloud.pubsub.v1.Publisher;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import com.google.protobuf.ByteString;
-import com.google.pubsub.v1.PubsubMessage;
-import com.google.pubsub.v1.TopicName;
-import org.apache.commons.io.IOUtils;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.runners.direct.DirectRunner;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Created by Bala on 04/07/2017.
@@ -40,46 +26,15 @@ public class PubSubEmitController {
 
     @RequestMapping("/generator")
     public String generator() throws Exception {
-        StorageOptions storageOptions = StorageOptions.newBuilder()
-                .setProjectId("exception-management").build();
-        Storage storage = storageOptions.getService();
+        DataflowPipelineOptions options = PipelineOptionsFactory.as(DataflowPipelineOptions.class);
+        options.as(DataflowPipelineOptions.class).setStreaming(true);
+        options.setProject("gcp-f2brb");
+        options.setStagingLocation("gs://f2brb-dataflow-staging/generator");
+        options.setRunner(DataflowRunner.class);
+        //options.setJobName("f2br-data-generation");
+        Pipeline pipeline = Pipeline.create(options);
 
-        String bucketName = "topic-source";
-        Bucket bucket = storage.get(bucketName);
-        Blob blob = bucket.create(
-                "GMMOException.xml", IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("GMMOException.xml")), "text/xml");
-
-
-        Blob readBlob = bucket.get("GMMOException.xml");
-        String blobContent = new String(readBlob.getContent(), UTF_8);
-        System.out.println("MY BLOB ===========" + blobContent);
-
-
-        try (Connection connection = BigtableConfiguration.connect("exception-management", "interstellar-travel")) {
-            Admin admin = connection.getAdmin();
-            Table table = connection.getTable(TableName.valueOf("dunkirk"));
-            Put put = new Put(Bytes.toBytes("MSG_1234567"));
-            put.addColumn(COLUMN_FAMILY_NAME, COLUMN_NAME, Bytes.toBytes(blobContent));
-            table.put(put);
-
-        } catch (IOException e) {
-            System.err.println("Exception while running GMMOException: " + e.getMessage());
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        Publisher publisher = null;
-        try {
-            TopicName topic = TopicName.create("exception-management", "CaptureTopic");
-            publisher = Publisher.defaultBuilder(topic).build();
-            ByteString data = ByteString.copyFromUtf8(blobContent);
-            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
-            publisher.publish(pubsubMessage);
-        } finally {
-            if (publisher != null) {
-                publisher.shutdown();
-            }
-        }
+        pipeline.run().waitUntilFinish();
         return "Message has been Generated, stowed in Cloud Storage and published in PubSub!!!";
     }
 }
--- src/main/java/com/sample/poc/PubSubEmitterApplication.java
+++ src/main/java/com/sample/poc/PubSubEmitterApplication.java
@@ -1,9 +1,5 @@
 package com.sample.poc;
 
-import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.sample.poc.model.Family;
-import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,13 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PubSubEmitterApplication {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(PubSubEmitterApplication.class, args);
 
-        /*JacksonXmlModule module = new JacksonXmlModule();
-        module.setDefaultUseWrapper(false);
-        XmlMapper xmlMapper = new XmlMapper(module);
-
-        System.out.println(xmlMapper.readValue(IOUtils.toByteArray(PubSubEmitterApplication.class.getClassLoader().getResourceAsStream("family.xml")), Family.class));*/
     }
 }
